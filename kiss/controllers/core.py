c1969081066ebc22c09d56afbf59afa04b0a8fd2
Stanislav Feldman <stanislavfeldman@gmail.com>
2012-06-11 11:37:06 +0800

    now if your controller doesn't have some http method will be executed default with 405 error

--- kiss/controllers/core.py
+++ kiss/controllers/core.py
@@ -1,4 +1,5 @@
 from putils.patterns import Singleton
+from kiss.views.core import Response
 
 
 class Controller(Singleton):
@@ -6,5 +7,15 @@ class Controller(Singleton):
 	Base class of all controllers.
 	It is just Singleton.
 	"""
-	pass
+	def get(self, request):
+		return Response("Method is not supported", status=405)
+		
+	def post(self, request):
+		return Response("Method is not supported", status=405)
+		
+	def put(self, request):
+		return Response("Method is not supported", status=405)
+		
+	def delete(self, request):
+		return Response("Method is not supported", status=405)
 
